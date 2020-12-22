dab95fea55dd6a5d0b4e80cb8c9a5e40d5085f66
lsteveol <slbridg3@gmail.com>
2020-12-22 07:48:21 -0800

    updated with polling attempts as a param

--- spi/gr_spi_agent.sv
+++ spi/gr_spi_agent.sv
@@ -1,12 +1,12 @@
 `ifndef GR_SPI_AGENT_SV
 `define GR_SPI_AGENT_SV
 
-class gr_spi_agent#(int ADDR_WIDTH=64) extends uvm_agent;
+class gr_spi_agent#(int ADDR_WIDTH=64, int polling_attempts=10) extends uvm_agent;
   
-  `uvm_component_utils_begin(gr_spi_agent)
+  `uvm_component_param_utils_begin(gr_spi_agent#(ADDR_WIDTH, polling_attempts))
   `uvm_component_utils_end
   
-  gr_spi_driver       driver;
+  gr_spi_driver#(ADDR_WIDTH, polling_attempts)       driver;
   gr_spi_sequencer    sequencer;
   gr_spi_monitor      monitor;
 
@@ -21,7 +21,7 @@ class gr_spi_agent#(int ADDR_WIDTH=64) extends uvm_agent;
     if(get_is_active() == UVM_ACTIVE) begin
 	    `uvm_info(get_type_name(), $psprintf("GR SPI Agent is UVM_ACTIVE"), UVM_MEDIUM);
       sequencer       = gr_spi_sequencer::type_id::create("sequencer", this);
-      driver          = gr_spi_driver#(ADDR_WIDTH)::type_id::create("driver", this);
+      driver          = gr_spi_driver#(ADDR_WIDTH, polling_attempts)::type_id::create("driver", this);
     end
   endfunction
 
